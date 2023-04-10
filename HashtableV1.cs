c2afda65ba599c8a3ac4dfb01d0e73ca6ea0e2e8
Oztturk <128193690+Oztturk@users.noreply.github.com>
2023-04-10 19:49:02 +0300

    Update HashtableV1.cs

--- HashtableV1.cs
+++ HashtableV1.cs
@@ -1,31 +1,34 @@
+using System;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ConsoleApp7
 {
     internal class Program
     {
         static Hashtable TABLE = new Hashtable();
-
         static void Main(string[] args)
         {
-
-            ekle("KEY","VAL");
-            ekle("KEY","VAL");
-            ekle("KEY","VAL");
+            ekle("E","R");
+            ekle("T","Y");
+            ekle("U","I");
 
             listele();
         }
 
         static void ekle(string key, string value)
         {
-            TABLE.Add(key, value); // hashtable'a metotdaki key ve value'yi ekler
+            TABLE.Add(key, value);
         }
 
         static void listele()
         {
             foreach (DictionaryEntry item in TABLE)
             {
-                Console.WriteLine(item.Key + ": " + item.Value); //foreach'den dönen veriyi yazdırır
+                Console.WriteLine(item.Key + ": " + item.Value);
             }
         }
     }
